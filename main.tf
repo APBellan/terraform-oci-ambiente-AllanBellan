0274f5dcd94e4039e935f625f6b7933d6dfc8a47
APBellan <allanbellan92@gmail.com>
2024-09-28 15:45:49 -0300

    Version 0.02

--- main.tf
+++ main.tf
@@ -1,28 +1,15 @@
-# Definição do módulo de instância
 module "oci_instance" {
-  source               = "oracle-terraform-modules/compute-instance/oci"
+  source               = "./modules/instance"
   instance_count       = 1
   ad_number            = 1
-  compartment_ocid     = var.compartment_ocid
+  compartment_ocid     = "ocid1.compartment.oc1..aaaaaaaaavr7olbbtghjmk4smaeoq5e4z2ykxazm6d5pmciae7z6ko73efoq"
   instance_display_name = "minha-instancia-teste"
-  source_ocid          = var.source_ocid  # OCID da imagem a ser usada para a instância
-  subnet_ocids         = var.subnet_ocids
-  public_ip            = "EPHEMERAL"  # Ou use "RESERVED" se preferir um IP público reservado
-  ssh_public_keys      = file("G:/Outros computadores/Meu laptop/Documents/Allan/DevOps/IAC/terraform-oci-ambiente-AllanBellan/.oci/InstanciaTeste/ssh-key-2023-01-18.key.pub")  # Caminho para a chave pública SSH
-  block_storage_sizes_in_gbs = [50]  # Anexar 50 GB de volume de bloco
-  shape                = "VM.Standard2.1"  # Tipo da instância
-  instance_state       = "RUNNING"
-}
-
-resource "oci_core_volume" "block_volume" {
-  availability_domain = var.ad_number
-  compartment_id      = var.compartment_ocid
-  size_in_gbs         = 50  # Tamanho do volume
-  display_name        = "volume-de-teste"
-}
-
-resource "oci_core_volume_attachment" "volume_attachment" {
-  instance_id = oci_core_instance.instance.id
-  volume_id   = oci_core_volume.block_volume.id
-  attachment_type  = "paravirtualized"
+  source_ocid          = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaa7oouirhxngn7emu3bq6kwoau2c365zipzlre5oat4zvpizrqsxwa"
+  subnet_ocids         = "ocid1.subnet.oc1.sa-saopaulo-1.aaaaaaaa4vwhwz7pirakqmuaxdnelkzoptoglf3i5rr5tdeytlf5fi6m43fa"
+  public_ip            = "EPHEMERAL" # NONE, RESERVED or EPHEMERAL
+  ssh_public_keys      = file("G:/Outros computadores/Meu laptop/Documents/Allan/DevOps/IAC/terraform-oci-ambiente-AllanBellan/.oci/InstanciaTeste/ssh-key-2023-01-18.key.pub")
+  block_storage_sizes_in_gbs = [50]
+  shape                = "VM.Standard2.1"
+  instance_state       = "RUNNING" # RUNNING or STOPPED
+  boot_volume_backup_policy  = "bronze" # disabled, gold, silver or bronze
 }
